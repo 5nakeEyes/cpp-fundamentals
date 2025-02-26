6ec4ca26e5cb4e75d14ec3c8dfece2673c0a0def
5nakeEyes <108435007+5nakeEyes@users.noreply.github.com>
2025-02-26 11:59:23 +0100

    calculate.hpp
    formatting fixes

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -3,25 +3,31 @@
 
 std::string calculate(const std::string& command, int first, int second) {
     
-    if(command == "add"){
+    if(command == "add")
+    {
         return std::to_string(first + second);
     }
     
-    if(command == "subtract") {
-	    return std::to_string(first - second);
+    if(command == "subtract")
+    {
+	return std::to_string(first - second);
     }
     
-    if(command == "multiply"){
+    if(command == "multiply")
+    {
         return std::to_string(first * second);
     }
     
-    if(command == "divide")  {
-	    if(second == 0) {
+    if(command == "divide")
+    {
+	if(second == 0)
+	{
             return "Division by 0";
-	    }
-	    else {
+	}
+	else
+	{
             return std::to_string(first / second);
-	    }
+	}
     }
     
     return "Invalid data";
