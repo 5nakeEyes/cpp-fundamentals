e9f402ab54c9db5e119956f79d8f1af4d55cc451
Maciej Cieplucha <snakeeyesplay@gmail.com>
2025-02-28 01:31:11 +0100

    NWD works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0)
+        return rhs;
+    if (rhs == 0)
+        return lhs;
+    if (lhs == 0 && rhs == 0)
+        return 0;
+    if (lhs < 0)
+        lhs *= -1;
+    if (rhs < 0)
+        rhs *= -1;
+    while (lhs != rhs)
+        if (lhs > rhs)
+            lhs -= rhs;
+        else
+            rhs -= lhs;
+    return rhs;
 }
 
 int NWW(int lhs, int rhs) {
