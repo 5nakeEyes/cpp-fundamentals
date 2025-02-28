6ed0eee2af47e22bdf47a5b733f6b6e90064d2cb
Maciej Cieplucha <snakeeyesplay@gmail.com>
2025-02-28 01:49:39 +0100

    NWW works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -20,6 +20,11 @@ int NWD(int lhs, int rhs) {
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0)
+        return 0;
+    if (lhs < 0)
+        lhs *= -1;
+    if (rhs < 0)
+        rhs *= -1;
+    return lhs * rhs / NWD(lhs, rhs);
 }
