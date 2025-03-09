5bfc5266550020ef4821061ef8c02c16744f0c13
Maciej Cieplucha <snakeeyesplay@gmail.com>
2025-03-09 01:54:50 +0100

    implement error code message function

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,2 +1,21 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode code) {
+    std::string msg = "";
+    switch (code) {
+    case ErrorCode::Ok: msg = "Ok\n";   
+        break;
+    case ErrorCode::PasswordNeedsAtLeastNineCharacters: msg = "Error: Password needs at least nine characters\n";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneNumber: msg = "Error: Password needs at least one number\n";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: msg = "Error: Password needs at least one special character\n";
+        break;
+    case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: msg = "Error: Password needs at least one uppercase letter\n";
+        break;
+    case ErrorCode::PasswordsDoNotMatch: msg = "Error: Passwords do not match\n";
+        break;
+    default: msg = "Unknown error!";
+    };
+    return msg;
+}
